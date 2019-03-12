652d04aca9542355e0b067facf1e7ecd59dffde1
Maxwell Hunter <MaxwellHunter@gmail.com>
2019-03-12 11:34:33 -0700

    Initial commit

---
+++ todo.py
@@ -0,0 +1,24 @@
+# TODO Use django-debug-toolbar to find places where database queries run too long
+# or hit the database too many times.
+
+# TODO Use django-debug-toolbar to find places where templates aren't properly using
+# inheritance.
+
+# TODO Check that models are using appropriate fields for the type of data they
+# store. If not, correct them and create migrations to handle the data.
+
+# TODO Check that forms are using the correct fields and validation. If not, fix.
+
+# TODO Use coverage.py to check the code coverage amount. 
+
+# TODO Write tests to increase test coverage to at least 75%.
+
+# EXTRA CREDIT: 
+# TODO Increase test coverage to 90% or above.
+# TODO Decrease combined query time on all views to 60ms or less.
+# TODO Add migrations to correct existing data when data types change.
+# TODO Add custom form validators
+
+# DONE:
+# Use the provided requirements.txt to install needed packages for the project
+# install django-debug-toolbar
